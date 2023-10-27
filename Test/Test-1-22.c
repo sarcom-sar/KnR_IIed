4f96e499feefe28e3931918f2733639a30509e78
Sara T <nil>
2023-10-28 01:28:56 +0200

    Small typo in test case

--- Test/Test-1-22.c
+++ Test/Test-1-22.c
@@ -8,7 +8,7 @@ void tearDown (void) {}
 void test_break_line_normal(void) {
   char line_normal[] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis\n";
   break_line(line_normal);
-  TEST_ASSERT_EQUAL_STRING("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Donec hendrerit tempor\ntellus.Donec pretium posuere tellus.Proin quam nisl, tincidunt et, mattis\n", line_normal);
+  TEST_ASSERT_EQUAL_STRING("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor\ntellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis\n", line_normal);
 }
 
 int main(void) {
