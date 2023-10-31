e84915ef2ea0384fa5b9693ea5b1a0ab522f89a9
Sara T <nil>
2023-10-31 14:42:51 +0100

    Tests for 1-22, which pass for some reason

--- Test/Test-1-22.c
+++ Test/Test-1-22.c
@@ -11,8 +11,30 @@ void test_break_line_normal(void) {
   TEST_ASSERT_EQUAL_STRING("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor\ntellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis\n", line_normal);
 }
 
+void test_break_line_too_short(void) {
+  char line_too_short[] = "A word\n";
+  break_line(line_too_short);
+  TEST_ASSERT_EQUAL_STRING("A word\n", line_too_short);
+}
+
+void test_break_line_long_word(void) {
+  char line_long_word[] =
+      "Aliquameratvolutpat. Nunceleifendleovitaemagna. "
+      "Inideratnonorcicommodolobortis. "
+      "Proinnequemassa,cursusut,gravidaut,lobortiseget,lacus. Seddiam. "
+      "Praesentfermentumtemportellus. Nullamtempus. "
+      "Maurisacfelisvelvelittristiqueimperdiet. Donecatpede. "
+      "Etiamvelnequenecduidignissimbibendum. Vivamusidenim. "
+      "Phasellusnequeorci,portaa,aliquetquis,sempera,massa. Phaselluspurus. "
+      "Pellentesquetristiqueimperdiettortor. Nameuismodtellusiderat.\n";
+  break_line(line_long_word);
+  TEST_ASSERT_EQUAL_STRING("Aliquameratvolutpat. Nunceleifendleovitaemagna. Inideratnonorcicommodolobortis.\nProinnequemassa,cursusut,gravidaut,lobortiseget,lacus. Seddiam.\nPraesentfermentumtemportellus. Nullamtempus. Maurisacfelisvelvelittristiqueimperdiet. Donecatpede.\nEtiamvelnequenecduidignissimbibendum. Vivamusidenim.\nPhasellusnequeorci,portaa,aliquetquis,sempera,massa. Phaselluspurus. Pellentesquetristiqueimperdiettortor.\nNameuismodtellusiderat.\n", line_long_word);
+}
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_break_line_normal);
+  RUN_TEST(test_break_line_too_short);
+  RUN_TEST(test_break_line_long_word);
   return UNITY_END();
 }
