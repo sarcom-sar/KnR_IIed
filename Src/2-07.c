03d24c4ddcbc2d1692e10413dea047a6a4c242f7
Sara T <nil>
2023-11-12 21:47:01 +0100

    Exercise 2-07 don

---
+++ Src/2-07.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+unsigned int invert(unsigned int x, int p, int n);
+
+int main(void) {
+  unsigned int x = 255; // b11111111
+  int p = 3; // 4th
+  int n = 3; // flip 3 bits to 000
+  printf("X = %u, p = %d, n = %d\n", x, p, n);
+  // 241 == b11110001
+  printf("X value: %u, expected value: %u\n", invert(x, p, n), 241);
+
+  /* --- */
+
+  x = 10; // b1010
+  p = 3; // 4th
+  n = 3;
+  printf("X = %u, p = %d, n = %d\n", x, p, n);
+  // 4 == b0101
+  printf("X value: %u, expected value: %u\n", invert(x, p, n), 4);
+
+  /* --- */
+
+  x = 14; // b1110
+  p = 0; // 1st
+  n = 1;
+  printf("X = %u, p = %d, n = %d\n", x, p, n);
+  // 15 == b1111
+  printf("X value: %u, expected value: %u\n", invert(x, p, n), 4);
+
+  return 0;
+}
+
+// returns xx...xnnnx...xxx
+// where position of first n is set by p
+unsigned int invert(unsigned int x, int p, int n) {
+  unsigned int xmask = ~0 << n;
+  unsigned int shift_by = p + 1 - n;
+  return x ^ (~xmask << shift_by);
+}
