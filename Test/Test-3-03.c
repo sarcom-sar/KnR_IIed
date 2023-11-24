12f34948f807bb790eada19eba72fa19d309bb08
Sara T <nil>
2023-11-25 00:43:13 +0100

    More tests for 3-03

--- Test/Test-3-03.c
+++ Test/Test-3-03.c
@@ -4,47 +4,82 @@
 void setUp(void) {}
 void tearDown(void) {}
 
-void test_expand_basic(void) {
-  char to_expand[1024] = "a-e";
-  char expanded[1024] = {0};
-  expand(to_expand, expanded);
-  TEST_ASSERT_EQUAL_STRING("abcde", expanded);
+void test_expand_forwards(void) {
+  char expanded_basic[1024] = {0};
+  expand("a-e", expanded_basic);
+  TEST_ASSERT_EQUAL_STRING("abcde", expanded_basic);
+
+  char expanded_twice[1024] = {0};
+  expand("a-e0-9", expanded_twice);
+  TEST_ASSERT_EQUAL_STRING("abcde0123456789", expanded_twice);
+
+  char expanded_within[1024] = {0};
+  expand("a-e-h", expanded_within);
+  TEST_ASSERT_EQUAL_STRING("abcdefgh", expanded_within);
+
+  char expanded_but_not_really[1024] = {0};
+  expand("a-b", expanded_but_not_really);
+  TEST_ASSERT_EQUAL_STRING("ab", expanded_but_not_really);
+}
+
+void test_expand_backwards(void) {
+  char expanded_backwards[1024] = {0};
+  expand("e-a", expanded_backwards);
+  TEST_ASSERT_EQUAL_STRING("edcba", expanded_backwards);
+
+  char expanded_twice_backwards[1024] = {0};
+  expand("e-a9-0", expanded_twice_backwards);
+  TEST_ASSERT_EQUAL_STRING("edcba9876543210", expanded_twice_backwards);
+
+  char expanded_backwards_within[1024] = {0};
+  expand("h-e-a", expanded_backwards_within);
+  TEST_ASSERT_EQUAL_STRING("hgfedcba", expanded_backwards_within);
+
+  char expanded_but_not_really[1024] = {0};
+  expand("z-y", expanded_but_not_really);
+  TEST_ASSERT_EQUAL_STRING("zy", expanded_but_not_really);
 }
 
 void test_expand_no_expansion(void) {
-  char to_expand[1024] = "abe";
-  char expanded[1024] = {0};
-  expand(to_expand, expanded);
-  TEST_ASSERT_EQUAL_STRING("abe", expanded);
+  char expanded_no_expansion[1024] = {0};
+  expand("abe", expanded_no_expansion);
+  TEST_ASSERT_EQUAL_STRING("abe", expanded_no_expansion);
+
+  char expanded_nothing[1024] = {0};
+  expand("", expanded_nothing);
+  TEST_ASSERT_EQUAL_STRING("", expanded_nothing);
 }
 
-void test_expand_twice(void) {
-  char to_expand[1024] = "a-e0-9";
-  char expanded[1024] = {0};
-  expand(to_expand, expanded);
-  TEST_ASSERT_EQUAL_STRING("abcde0123456789", expanded);
+void test_expand_leading_trailing(void) {
+  char expanded_leading[1024] = {0};
+  expand("-a", expanded_leading);
+  TEST_ASSERT_EQUAL_STRING("-a", expanded_leading);
+
+  char expanded_trailing[1024] = {0};
+  expand("a-", expanded_trailing);
+  TEST_ASSERT_EQUAL_STRING("a-", expanded_trailing);
 }
 
-void test_expand_leading(void) {
-  char to_expand[1024] = "-a-e";
-  char expanded[1024] = {0};
-  expand(to_expand, expanded);
-  TEST_ASSERT_EQUAL_STRING("-abcde", expanded);
-}
+void test_expand_corner_case(void) {
+  char expanded_newline[1024] = {0};
+  expand("\n-a", expanded_newline);
+  TEST_ASSERT_EQUAL_STRING("\n-a", expanded_newline);
 
-void test_expand_trailing(void) {
-  char to_expand[1024] = "a-e-h";
-  char expanded[1024] = {0};
-  expand(to_expand, expanded);
-  TEST_ASSERT_EQUAL_STRING("abcde-h", expanded);
+  char expanded_hyphen[1024] = {0};
+  expand("--a", expanded_hyphen);
+  TEST_ASSERT_EQUAL_STRING("--a", expanded_hyphen);
+
+  char expanded_alnum[1024] = {0};
+  expand("A-8", expanded_alnum);
+  TEST_ASSERT_EQUAL_STRING("A98", expanded_alnum);
 }
 
 int main(void) {
   UNITY_BEGIN();
-  RUN_TEST(test_expand_basic);
+  RUN_TEST(test_expand_forwards);
+  RUN_TEST(test_expand_backwards);
   RUN_TEST(test_expand_no_expansion);
-  RUN_TEST(test_expand_twice);
-  RUN_TEST(test_expand_leading);
-  RUN_TEST(test_expand_trailing);
+  RUN_TEST(test_expand_leading_trailing);
+  RUN_TEST(test_expand_corner_case);
   return UNITY_END();
 }
