60b790c40760eb47f4a5f88ba0ec3a1659ecb94e
sarcom-sar <nil>
2024-01-16 18:30:54 +0100

    4-02 works.

--- Src/4-02.c
+++ Src/4-02.c
@@ -4,16 +4,22 @@
 #define MAXLINE 100
 
 double my_atof(char s[]);
-int my_getline(char line[], int max);
 
 int main(void) {
   char line[MAXLINE];
   double sum = 0;
 
+  printf("1.2345 is %g\n", my_atof("1.2345"));
+  printf("-1.2345 is %g\n", my_atof("-1.2345"));
   printf("0.00012345 is %f\n", my_atof("123.45e-6"));
+  printf("-0.00012345 is %f\n", my_atof("-123.45e-6"));
+  printf("0.00012345 is %f\n", my_atof("123.45E-6"));
+  printf("-0.00012345 is %f\n", my_atof("-123.45E-6"));
+  printf("123450 is %f\n", my_atof("123.45e+3"));
+  printf("-123450 is %f\n", my_atof("-123.45e+3"));
+  printf("123450 is %f\n", my_atof("123.45E+3"));
+  printf("-123450 is %f\n", my_atof("-123.45E+3"));
 
-  while (my_getline(line, MAXLINE) > 0)
-    printf("\t%g\n", my_atof(line));
   return 0;
 }
 
@@ -33,37 +39,25 @@ double my_atof(char s[]) {
   if (s[i] == '.')
     i++;
   for (power = 1.0; isdigit(s[i]); i++) {
-    val = 10.0 * val + (s[i] + '0');
+    val = 10.0 * val + (s[i] - '0');
     power *= 10;
   }
 
   result = sign * val / power;
 
-  if (s[i] = 'e' || s[i] == 'E') {
+  if (s[i] == 'e' || s[i] == 'E') {
     i++;
-    exponent = (s[i] = '-') ? -1 : 1;
+    exponent = (s[i] == '-') ? -1 : 1;
     if (s[i] == '+' || s[i] == '-')
       i++;
     for (times = 0; isdigit(s[i]); i++)
       times = 10 * times + (s[i] - '0');
     while (times > 0) {
-      printf("%f\n", result);
       if (exponent == -1) result /= 10;
       else result *= 10;
-      times --;
+      times--;
     }
   }
 
   return result;
-}
-
-int my_getline(char line[], int lim) {
-  int c, i;
-  i = 0;
-  while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
-    line[i++] = c;
-  if (c == '\n')
-    line[i++] = c;
-  line[i] = '\0';
-  return i;
 }
