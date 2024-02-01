db1872096b90c0196cd10b09ff5872aa279706a2
sarcom-sar <nil>
2024-02-01 01:14:32 +0100

    Since BUFSIZE is 1, only one character is read

---
+++ Src/4-08.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUFSIZE 1
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void) {
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+  if (bufp > BUFSIZE)
+    printf("error, stack is full\n");
+  else
+    buf[bufp++] = c;
+}
+
+void ungets(char s[]) {
+  int i = strlen(s);
+  while (i > 0)
+    ungetch(s[--i]);
+}
+
+int main() {
+  char s[] = "I am just some string\n";
+  int c;
+
+  ungets(s);
+  while ((c = getch()) != EOF)
+    putchar(c);
+}
+
